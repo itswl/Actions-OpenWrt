23dcde955365b641ce5a9c9139eff78d63a166fb
WeiLai <imwl@live.com>
2020-04-20 19:34:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,6 +11,8 @@
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
 sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_generate
